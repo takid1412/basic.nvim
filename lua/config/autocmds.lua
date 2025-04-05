edd0d8e415ad82778b10226253dd791b9fa70cfd
takid1412 <duanpq92@gmail.com>
2025-04-05 16:29:08 +0700

    fix padding

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,3 +10,17 @@ vim.api.nvim_create_autocmd("FileType", {
     ]])
   end
 })
+vim.api.nvim_create_autocmd({'UIEnter', 'ColorScheme'}, {
+    callback = function()
+        local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+        if normal.bg then
+            io.write(string.format('\027]11;#%06x\027\\', normal.bg))
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd('UILeave', {
+    callback = function()
+        io.write('\027]111\027\\')
+    end,
+})
